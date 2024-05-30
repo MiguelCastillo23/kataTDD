538c25b0c7b047d588305fd87a5ce13a939177f7
MiguelCastillo23 <7842132@continental.edu.pe>
2024-05-30 18:32:55 -0500

    Caso 2: Calcular promedio con un solo valor (Rojo)

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -5,7 +5,8 @@ class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio_retornaNone(self):
         conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())  # add assertion here
-
-
+    def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+        conjunto = Conjunto([5])
+        self.assertEqual(5, conjunto.promedio())
 if __name__ == '__main__':
     unittest.main()
